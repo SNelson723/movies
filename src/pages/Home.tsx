36e0c0a59a716a99fc6f7ddb20682f64503bdf17
SNelson723 <stephenn649@gmail.com>
2025-01-27 22:15:35 -0600

    Added basic search elements, moving on to sending axios requests to the movide databse api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
-
+import { useState } from "react";
+import axios from 'axios';
 
 const Home = () => {
+  const [text, setText] = useState<string>("");
+
   return (
-    <div>
-      Home page on the right
+    <div className="w-[calc(100vw-150px)] h-[100vh] my-5">
+      <div className="flex justify-center">
+        <div>
+          <div>
+            <p className="text-center py-2">Seach by Movie Title</p>
+          </div>
+          <input
+            className="py-1 rounded-l-md"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.currentTarget.value)}
+          />
+          <button className="border rounded-r-md px-2 py-1">Search</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Home;
