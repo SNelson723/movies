e00e702e41572b9f4ccf16a899bbe3d10c8af95d
SNelson723 <stephenn649@gmail.com>
2025-01-27 23:53:07 -0600

    Got the first draft of the movie info card, I need to fix the background and styling of it, but I can make it into its own component after that gets taken care of

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <>
-      <div className="w-screen h-screen bg-gradient-to-r from-indigo-600 to-emerald-500 flex">
-        <SideBar />
-        <div>
-          <Outlet />
-        </div>
+    <div className="w-screen h-screen bg-gradient-to-r from-indigo-600 to-emerald-500 flex overflow-hidden">
+      <SideBar />
+      <div className="flex-1 overflow-auto no-scrollbar">
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
 
