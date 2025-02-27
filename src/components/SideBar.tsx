1bf2c276e68f8cc4a5623318d32390a8fb725f68
SNelson723 <stephenn649@gmail.com>
2025-02-27 01:03:59 -0600

    Fixed a small issue in sidebar layout

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,6 +5,8 @@ const SideBar = () => {
     <div className="text-fuchsia-100 w-[150px] max-w-[150px] border-r border-r-emerald-500">
       <div className="w-full p-3 border-b border-b-emerald-500 hover:bg-emerald-400 hover:text-slate-800 transition-all duration-200">
         <NavLink to="/">Home</NavLink>
+      </div>
+      <div className="w-full p-3 border-b border-b-emerald-500 hover:bg-emerald-400 hover:text-slate-800 transition-all duration-200">
         <NavLink to="/favorites">My Favorites</NavLink>
       </div>
     </div>
